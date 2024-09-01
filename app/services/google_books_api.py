3383a48306d8b0658e572ca1c8c9cb04e27b410c
Paolo Gomez <peweegardner248@gmail.com>
2024-09-01 15:07:36 -0700

    update

--- app/services/google_books_api.py
+++ app/services/google_books_api.py
@@ -1,6 +1,27 @@
 import requests
 from flask import current_app
 
+def search_books(query, max_results=10):
+    api_key = current_app.config['GOOGLE_BOOKS_API_KEY']
+    url = f'https://www.googleapis.com/books/v1/volumes?q={query}&maxResults={max_results}&key={api_key}'
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    books = []
+    for item in data.get('items', []):
+        volume_info = item.get('volumeInfo', {})
+        books.append({
+            'id': item['id'],
+            'title': volume_info.get('title', 'Unknown')[:500],
+            'authors': ', '.join(volume_info.get('authors', ['Unknown']))[:200],
+            'published_date': volume_info.get('publishedDate', 'Unknown')[:20],
+            'description': volume_info.get('description', 'No description available')[:5000],
+            'image_link': volume_info.get('imageLinks', {}).get('thumbnail', '')[:500]
+        })
+    
+    return books
+
 def get_book_details(book_id):
     api_key = current_app.config['GOOGLE_BOOKS_API_KEY']
     url = f'https://www.googleapis.com/books/v1/volumes/{book_id}?key={api_key}'
@@ -15,9 +36,9 @@ def get_book_details(book_id):
     
     return {
         'id': data['id'],
-        'title': volume_info.get('title', 'Unknown')[:500],  # Limit to 500 characters
-        'authors': ', '.join(volume_info.get('authors', ['Unknown']))[:200],  # Limit to 200 characters
-        'published_date': volume_info.get('publishedDate', 'Unknown')[:20],  # Limit to 20 characters
-        'description': volume_info.get('description', 'No description available')[:5000],  # Limit to 5000 characters
-        'image_link': volume_info.get('imageLinks', {}).get('thumbnail', '')[:500]  # Limit to 500 characters
+        'title': volume_info.get('title', 'Unknown')[:500],
+        'authors': ', '.join(volume_info.get('authors', ['Unknown']))[:200],
+        'published_date': volume_info.get('publishedDate', 'Unknown')[:20],
+        'description': volume_info.get('description', 'No description available')[:5000],
+        'image_link': volume_info.get('imageLinks', {}).get('thumbnail', '')[:500]
     }
