4ae1f1113b64a208f5925ed39fe165245dd4a765
Paolo Gomez <peweegardner248@gmail.com>
2024-09-01 19:31:56 -0700

    Revert "Update models.py"
    This reverts commit d0f4ea056048c4bd25ba0e2490e92a94d6a3a1e1.

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login_manager
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
